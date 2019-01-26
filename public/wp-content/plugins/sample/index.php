15b9cb5bfa9f51c2dd32a60d94a396f7b4dcc5dd
yat8823jp <yat@yat-net.com>
2019-01-27 01:00:35 +0900

    add-sample-plugin

---
+++ public/wp-content/plugins/sample/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Short description
+ *
+ * @package sample
+ * @version 0.0.1
+ */
+
+/*
+Plugin Name: sample
+Plugin URI: https://example.com
+Description: sample
+Author: YAT
+Version: 0.0.1
+Text Domain: sample
+*/
